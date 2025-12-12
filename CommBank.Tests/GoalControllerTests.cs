5f65c5e4eddd6e59c5d6e09a132db20bc50d7833
Bright Omari Owusu <188401332+omariomari2@users.noreply.github.com>
2025-12-12 03:15:04 -0600

    postman-tests

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -65,10 +65,24 @@ public class GoalControllerTests
     [Fact]
     public async void GetForUser()
     {
-        // Arrange
-        
-        // Act
-        
-        // Assert
+        var goals = collections.GetGoals();
+        var users = collections.GetUsers();
+        IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+        IUsersService usersService = new FakeUsersService(users, users[0]);
+        GoalController controller = new(goalsService, usersService);
+
+        var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+        controller.ControllerContext.HttpContext = httpContext;
+        var result = await controller.GetForUser(users[0].Id!);
+
+        Assert.NotNull(result);
+        var index = 0;
+        foreach (Goal goal in result)
+        {
+            Assert.IsAssignableFrom<Goal>(goal);
+            Assert.Equal(goals[index].Id, goal.Id);
+            Assert.Equal(goals[index].Name, goal.Name);
+            index++;
+        }
     }
 }
